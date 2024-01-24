648ea3b418c77b81d613805cda7d625746b862dc
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-01-24 09:52:53 -0300

    feat: created new repository UsuarioRepository and model UsuarioLogin

--- src/main/java/com/generation/mercadodelas/repository/UsuarioRepositoy.java
+++ src/main/java/com/generation/mercadodelas/repository/UsuarioRepositoy.java
@@ -10,5 +10,5 @@ import com.generation.mercadodelas.model.Usuario;
 @Repository
 public interface UsuarioRepositoy extends JpaRepository<Usuario, Long> {
 
-		public Optional<Usuario> findByUsuario(String usuario);
+		public Optional<Usuario> findByUsuario(String usuario); ///teste
 }
